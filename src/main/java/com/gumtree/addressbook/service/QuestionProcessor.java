f5dfed747721e09b9e86992ec0e93c5c9a1e03f8
Simon Jenkins <simon.jenkins@o2.com>
2018-02-07 08:48:06 +0000

    Question processor tests

--- src/main/java/com/gumtree/addressbook/service/QuestionProcessor.java
+++ src/main/java/com/gumtree/addressbook/service/QuestionProcessor.java
@@ -6,12 +6,17 @@ import com.gumtree.addressbook.resolver.Resolver;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.String.format;
+
 public class QuestionProcessor
 {
     private Map<String, Resolver> resolvers;
     String process(String question, String resolverKey, List<AddressBookEntry> data, String... parms)
     {
         Resolver resolver = resolvers.get(resolverKey);
+        if (resolver == null) {
+            return format("I cannot answer this question - no resolver found for key: %s", resolverKey);
+        }
         return resolver.answer(data, parms);
     }
 
--- src/test/java/com/gumtree/addressbook/service/QuestionProcessorTest.java
+++ src/test/java/com/gumtree/addressbook/service/QuestionProcessorTest.java
@@ -1,14 +1,54 @@
 package com.gumtree.addressbook.service;
 
+import com.gumtree.addressbook.domain.AddressBookEntry;
+import com.gumtree.addressbook.domain.Sex;
+import com.gumtree.addressbook.resolver.SexCounter;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collections.*;
 import static org.junit.Assert.*;
 
 public class QuestionProcessorTest
 {
+    private static QuestionProcessor qp = new QuestionProcessor();
     @Test
-    public void process() throws Exception
+    public void testProcess() throws Exception
     {
+        List<AddressBookEntry> data = new ArrayList<>();
+        AddressBookEntry entry = new AddressBookEntry();
+        entry.setSex(Sex.MALE);
+        data.add(entry);
+
+        entry = new AddressBookEntry();
+        entry.setSex(Sex.FEMALE);
+        data.add(entry);
+
+        entry = new AddressBookEntry();
+        entry.setSex(Sex.FEMALE);
+        data.add(entry);
+
+        qp.setResolvers(singletonMap("sex.counter", new SexCounter()));
+
+        String answer = qp.process("How many females?", "sex.counter", data, "FEMALE");
+        assertTrue(answer.contains("2 female"));
     }
+
+    public void testResolverMissing()
+    {
+        List<AddressBookEntry> data = new ArrayList<>();
+        AddressBookEntry entry = new AddressBookEntry();
+        entry.setSex(Sex.MALE);
+        data.add(entry);
+
+        qp.setResolvers(emptyMap());
+
+        String answer = qp.process("How many females?", "sex.counter", data, "FEMALE");
+        assertTrue(answer.contains("I cannot answer this question"));
+    }
+
 
 }
